c683101b2e1bd66e6c9e11aa63481a1efaf63ffc
Kinza Yousaf <126621384+kinza-yousaf@users.noreply.github.com>
2026-06-25 11:16:44 -0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        parts = []
+
+        def isPali(s, i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i, j = i +1, j - 1
+            return True
+        
+        def dfs(i):
+            if i == len(s):
+                res.append(parts.copy())
+                return
+
+            for j in range(i, len(s)):
+                if isPali(s, i, j):
+                    parts.append(s[i: j + 1])
+                    dfs(j + 1)
+                    parts.pop()
+
+        
+        dfs(0)
+        return res
+
+        
