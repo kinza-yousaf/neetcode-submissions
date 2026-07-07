de9a787dc50502e1865aef213fca3907e39fe3e1
Kinza Yousaf <126621384+kinza-yousaf@users.noreply.github.com>
2026-07-07 19:58:26 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(i, node):
+            if i == len(word):
+                return node.endOfWord
+            c = word[i]
+            if c == ".":
+                for childNode in node.children.values():
+                    if dfs(i + 1, childNode):
+                        return True
+                return False
+            else:
+                if c not in node.children:
+                        return False
+                return dfs(i + 1, node.children[c])
+        return dfs(0, self.root)
+                
